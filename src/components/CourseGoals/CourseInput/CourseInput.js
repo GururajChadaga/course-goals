50116924215d1e2496a7ba07eb6e639eaabd4816
gururajchadaga <guruuraj.chadaga@gmail.com>
2021-08-03 22:19:22 +0530

    Fixed empty input check and added feedback styles

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -3,31 +3,37 @@ import Button from '../../UI/Button/Button';
 import { useState } from 'react';
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
   const courseInputChangeHandler = (event) => {
+    setIsValid(true);
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
-    if (enteredValue.length !== 0) {
-      props.onAddCourseGoal(enteredValue);
-      setEnteredValue('');
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
     }
+    props.onAddCourseGoal(enteredValue);
+    setEnteredValue('');
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className='form-control'>
-        <label>CourseGoal</label>
+        <label style={{ color: isValid ? 'black' : 'red' }}>CourseGoal</label>
         <input
+          style={{
+            borderColor: isValid ? '#cc' : '#red',
+            backgroundColor: isValid ? 'transparent' : 'salmon',
+          }}
           value={enteredValue}
           onChange={courseInputChangeHandler}
           type='text'
         />
       </div>
-      <Button type='submit'>
-        Add Goal
-      </Button>
+      <Button type='submit'>Add Goal</Button>
     </form>
   );
 };
